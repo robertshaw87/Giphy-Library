141bfbe2ae33f0c548c3764e13673a48de8387cb
Robert Shaw <robertshaw87@gmail.com>
2018-05-04 22:37:10 -0700

    add working button adding

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,5 +1,6 @@
 var tagLibrary = ["cat", "kitten", "puppy"];
 
+// display all the values of the tagLibrary as buttons in the buttons-area div
 function displayButtons() {
     $("#buttons-area").empty();
     for (var i=0; i<tagLibrary.length; i++) {
@@ -7,10 +8,23 @@ function displayButtons() {
         newButton.addClass("btn btn-outline-secondary");
         newButton.attr("type", "button");
         newButton.text(tagLibrary[i]);
-        newButton.attr("data-name", tagLibrary[i]);
+        newButton.data("name", tagLibrary[i]);
         $("#buttons-area").prepend(newButton);
     }
 }
+
+$(document).on("click", "#add-button", function(event) {
+    event.preventDefault();
+    // This line of code will grab the input from the input area
+    var newQuery = $("#user-input").val().trim();
+    // clear the input area
+    $("#user-input").val("");
+    tagLibrary.push(newQuery);
+
+    // Calling renderButtons which handles the processing of our movie array
+    displayButtons();
+
+  });
 
 $(document).ready(function() {
     displayButtons()
